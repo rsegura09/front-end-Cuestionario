841c85b36bec72cb5b8e10adcd90fa4d8a4866e8
Ricardo Segura <seguraricardo380@gmail.com>
2024-05-13 13:10:50 -0500

    feat: Creación del navbar

---
+++ src/app/pages/crearcuestionario/crearcuestionario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crearcuestionario',
+  standalone: true,
+  imports: [],
+  templateUrl: './crearcuestionario.component.html',
+  styleUrl: './crearcuestionario.component.scss'
+})
+export class CrearcuestionarioComponent {
+
+}
---
+++ src/app/pages/cuestionario/cuestionario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuestionario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuestionario.component.html',
+  styleUrl: './cuestionario.component.scss'
+})
+export class CuestionarioComponent {
+
+}
