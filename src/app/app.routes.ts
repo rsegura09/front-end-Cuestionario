38680f89411b70e0c01b5ce3649aeef8cdc5472e
Ricardo Segura <seguraricardo380@gmail.com>
2024-05-13 18:53:08 -0500

    feat: Se crea interfaz CrearCuestionario y Cuestionario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CrearcuestionarioComponent } from './pages/crearcuestionario/crearcuestionario.component';
+import { Component } from '@angular/core';
+import { CuestionarioComponent } from './pages/cuestionario/cuestionario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'crearcuestionario', component: CrearcuestionarioComponent},
+  {path: 'cuestionario', component: CuestionarioComponent}
+];
--- src/app/pages/cuestionario/cuestionario.component.ts
+++ src/app/pages/cuestionario/cuestionario.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-cuestionario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cuestionario.component.html',
-  styleUrl: './cuestionario.component.scss'
+  styleUrl: './cuestionario.component.scss',
 })
 export class CuestionarioComponent {
+  cuestionarioForm = this.formBuilder.group({
+    titulo: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    fechaDeEjecucion: ['', Validators.required],
+    horaDeEjecucion: ['', Validators.required],
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  enviar() {
+    console.warn(this.cuestionarioForm.value.titulo);
+  }
 }
