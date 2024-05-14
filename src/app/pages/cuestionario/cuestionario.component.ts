a7ce756c420af8fe8f0c9dda1a2f3db9f0b4b889
Ricardo Segura <seguraricardo380@gmail.com>
2024-05-13 23:21:29 -0500

    feat: Servicio de la api Cuestionario conexión Ok

--- src/app/pages/cuestionario/cuestionario.component.ts
+++ src/app/pages/cuestionario/cuestionario.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  ReactiveFormsModule,
-  FormGroup,
-  FormControl,
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ApiCuestionarioService } from '../../service/api-cuestionario.service';
+import { ICuestionario } from '../../models/cuestionario.model';
 
 @Component({
   selector: 'app-cuestionario',
@@ -14,7 +10,10 @@ import {
   templateUrl: './cuestionario.component.html',
   styleUrl: './cuestionario.component.scss',
 })
-export class CuestionarioComponent {
+export class CuestionarioComponent implements OnInit {
+
+  cuestionarioLista: ICuestionario[] = [];
+
   cuestionarioForm = this.formBuilder.group({
     titulo: ['', Validators.required],
     descripcion: ['', Validators.required],
@@ -22,9 +21,31 @@ export class CuestionarioComponent {
     horaDeEjecucion: ['', Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private _apiCuestionario: ApiCuestionarioService
+  ) {}
+
+  ngOnInit(): void {
+    this.getCuestionarios();
+  }
+
+  getCuestionarios() {
+    this._apiCuestionario.getAllCuestionario().subscribe({
+      next: (result) => {
+        this.cuestionarioLista = result;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
   enviar() {
     console.warn(this.cuestionarioForm.value.titulo);
+  }
+
+  listarCuestionarios() {
+    console.log(this.cuestionarioLista);
   }
 }
---
+++ src/app/service/api-cuestionario.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICuestionario } from '../models/cuestionario.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiCuestionarioService {
+  private baseUrl = 'https://localhost:44362/api';
+
+  constructor(private http: HttpClient) {}
+
+  getAllCuestionario(): Observable<ICuestionario[]> {
+    return this.http.get<ICuestionario[]>(`${this.baseUrl}/cuestionarios`);
+  }
+
+  getCuestionarioById(id: Number): Observable<ICuestionario> {
+    return this.http.get<ICuestionario>(`${this.baseUrl}/cuestionarios/${id}`);
+  }
+
+  postCuestionario(cuestionario: ICuestionario): Observable<ICuestionario> {
+    return this.http.post<ICuestionario>(
+      `${this.baseUrl}/cuestionarios`,
+      cuestionario
+    );
+  }
+}
